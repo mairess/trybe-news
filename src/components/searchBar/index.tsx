788a24c4594e3e41ddab6b343c49228b5282e145
mairess <trabalho.maires@gmail.com>
2023-11-25 21:11:17 -0300

    style: positioning searchBar

--- src/components/searchBar/index.tsx
+++ src/components/searchBar/index.tsx
@@ -1,17 +1,24 @@
 import { useContext } from 'react';
-import { Input } from './style';
+import { GoSearch } from 'react-icons/go';
+import { Input, Container, Wrapper } from './style';
 import NewsContext from '../../context/NewsContext';
 
 function SearchBar() {
   const { handleChange } = useContext(NewsContext);
 
   return (
-    <Input
-      onChange={ handleChange }
-      type="text"
-      name=""
-      id=""
-    />
+    <Container>
+      <Wrapper>
+        <GoSearch />
+      </Wrapper>
+      <Input
+        placeholder="Busque aqui"
+        onChange={ handleChange }
+        type="text"
+        name=""
+        id=""
+      />
+    </Container>
   );
 }
 
--- src/components/searchBar/style.ts
+++ src/components/searchBar/style.ts
@@ -1,10 +1,24 @@
 import styled from 'styled-components';
 
-export const Input = styled.input`
-    width: 40%;
-    height: 1.5rem;
-
-    @media screen and (max-width: 480px) {
-            width: 90%;
+export const Container = styled.div`
+    position: relative;
+    width: 60%;
+    @media screen and (min-width: 680px) {
+        width: 50%;
     }
+`;
+
+export const Wrapper = styled.div`
+    position: absolute;
+    left: 10px;
+    top: 5px;
+    display: flex;
+    align-items: center;
+`;
+
+export const Input = styled.input`
+    height: 1.5rem;
+    border-radius: 1rem;
+    padding: 0.7rem 1.6rem;
+    width: 100%;
 `;
